c9425aef7e529c40a4aba8ee588ce35778b3b6c5
Gerasimov Ruslan <gerassimov.ruslan@gmail.coom>
2020-03-21 21:04:54 +0300

    initial commit

---
+++ tests/lib/rules/map.js
@@ -0,0 +1,111 @@
+/**
+ * @fileoverview lodash map to Array.map
+ * @author Ruslan
+ */
+"use strict";
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+var rule = require("../../../lib/rules/map"),
+
+    RuleTester = require("eslint").RuleTester;
+
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+var ruleTester = new RuleTester();
+ruleTester.run("map", rule, {
+
+    valid: [
+        `test.map(function(item) { return item + 22; })`,
+        `if(Array.isArray(collection)) {
+            collection.map(function(item) {
+                return item + 22;
+            })
+        } else {
+            _.map(collection, function(item) {
+                return item + 22;
+            });
+        }`,
+        `if(Array.isArray(collection)){ 
+            collection.map(function(item) {
+                return item + 22; 
+            })
+        } else if (true){
+            _.map(collection, function(item) { 
+                return item + 22; 
+            })
+        }`,
+        `if(Array.isArray(collection)){
+            collection.map(function(item) { 
+                return item + 22; 
+            })
+        } else {
+            _.map(collection, function(item) { 
+                return item + 22; 
+            })
+        }`,
+
+        `if(Array.isArray(collection)){
+            collection.map(function(item) { 
+                return item + 22; })
+        } else if(false) {
+            _.map(collection, function(item) { 
+                   return item + 22; 
+            })
+        } else if(true) {
+            _.map(collection, function(item) { 
+                return item + 22; 
+            })
+        }`,
+    ],
+
+    invalid: [
+        {
+            code: `_.map(c1, function (item) { item + 22; })`,
+            errors: [{
+                message: "You probably need to use Array.map method on c1"
+            }]
+        },
+        {
+            code:
+                `function test() {
+                    if(false) {
+                        return collection.map(function(item) {
+                            return item + 2;
+                        })
+                    } else {
+                        return _.map(collection, function(item) {
+                            return item + 2;
+                        })
+                    }
+                }`,
+            errors: [{
+                message: "You probably need to use Array.map method on collection"
+            }]
+        },
+        {
+            code:
+                `if(!Array.isArray(collection)){
+                    collection.map(function(item) { 
+                        return item + 22; })
+                } else if(false) {
+                    _.map(collection, function(item) { 
+                           return item + 22; 
+                    })
+                } else if(true) {
+                    _.map(collection, function(item) { 
+                        return item + 22; 
+                    })
+                }`,
+            errors: [
+                { message: "You probably need to use Array.map method on collection" },
+                { message: "You probably need to use Array.map method on collection" }
+            ]
+        }
+    ]
+});
